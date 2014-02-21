a824ee7a795159454deb9ffc741a05bf3cfe30d6
s-tsubosaki <s-tsubosaki@nbsi.jp>
2014-02-21 01:30:34 +0000

    #20

--- site-cookbooks/nginx/recipes/default.rb
+++ site-cookbooks/nginx/recipes/default.rb
@@ -41,9 +41,9 @@ else
 
 template "nginx.conf" do
   # デフォルトのインストール先になる
-  #path "/etc/nginx/nginx.conf"
+  path "/etc/nginx/nginx.conf"
   # nginx.conf.erbが自動選択される
-  #source "nginx.conf.erb"
+  source "nginx.conf.erb"
   owner "root"
   group "root"
   mode 0644
